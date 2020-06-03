94cf0784ab424a8fb1e41c1ef289af017c69dbe3
ragnarr18 <ragnarr18@skel.ru.is>
2020-06-03 14:11:18 +0000

    json working

--- reactWebsite/App/src/Shop.js
+++ reactWebsite/App/src/Shop.js
@@ -19,7 +19,8 @@ class Shop extends React.Component {
         this.fetchItems()
     }
 
-    fetchItems = async() => {
+    // fetchItems = async() => {
+    async fetchItems(_reagion){
         const data = await fetch('https://api.quarantine.country/api/v1/spots/region?region=Iceland');
         const items = await data.json();
         const dates = items.data;
@@ -27,20 +28,40 @@ class Shop extends React.Component {
         let keys = Object.keys(dates);
         //let vals = Object.values(dates);
         let entries = Object.entries(dates);
-        console.log(entries)
-        this.setState({keys : keys})
-        //setItems(keys);
+        let values = Object.values(dates);
+        console.log("this is the entries" + entries)
+        console.log("this is the first entry: " + Object.values(dates))
+        console.log("my ultimate value: " + Object.values(dates["2020-06-03"]));
+        this.setState({keys : keys, dates: dates, currentDate: 0})
+        
+        console.log(this.state.keys[0]);
+       
     }
 
-    // function currentDate() {
+    currentDate() {
+        // let curr = this.state.currentDate;
+        // console.log(curr)
+        // let currkey = this.state.keys[curr];
+        // let values = Object.values(this.state.dates[currkey])
+        // //let myval = Object.values(this.state.dates[this.state])°
+        if(this.state.dates  != undefined) {
+        // if (this.state.dates != undefined){
+            let onlyValues = Object.values(this.state.dates[this.state.keys[this.state.currentDate]]);
+            let myJson = this.state.dates[this.state.keys[this.state.currentDate]];
+            let retString = JSON.stringify(myJson)
+            return retString
+        }
+        return
+        
+    }
 
-    // }
     render () {
     return (
         // <div>
         //     <h1>Shop page</h1>
             <div>
-                {this.state.keys}
+                {this.currentDate()}
+                {/* {this.state.keys} */}
                 <Date date="bla"/>
             </div>
         // </div>
