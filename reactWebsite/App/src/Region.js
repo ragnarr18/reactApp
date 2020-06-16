fff5e78a481274eaeea2fbbd4b68504fd10f5349
ragnarr18 <ragnarr18@skel.ru.is>
2020-06-16 21:29:36 +0000

    update, better usage of data

--- reactWebsite/App/src/Region.js
+++ reactWebsite/App/src/Region.js
@@ -3,46 +3,119 @@ import SingleRegion from './SingleRegion';
 class Region extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: '', dateA: 0, dateB: 1, dataAvailable: false};
+        this.state = {dates: '', dateIndexA: 0, dateIndexB: 1, dataAvailable: false, dataA: "", dataB:""};
+        this.SingleRegionaA = React.createRef();
+        this.SingleRegionaB = React.createRef();
     }
 
     componentDidMount(){
         const query = new URLSearchParams(this.props.location.search);
         this.state.value = query.get("region")
-        console.log("this is the state value: " + this.state.value)
-        this.callAPI(this.state.value, function(data){
-            return data
-        })
+        // console.log("this is the state value: " + this.state.value)
+        // this.callAPI.bind(this);
+        
+        this.callAPI(this.state.value)
+        // function (data){
+            // this.setState(data);
+            // return;
+        //}
+        //)
     }
 
 
-    async callAPI(regionName, callback) {
+    async callAPI(regionName) {
+        //this.callback.bind(this)
         console.log("this is the regionName: " + regionName);
         console.log("starting to fetch");
         const data = await fetch("http://localhost:9000/testAPI/region" + "/" + regionName);
         console.log("this is the REGION: " + regionName);
         const dates = await data.json();
         if (data.status >= 400){
-            this.setState({value: dates.message})
-            callback(this.state.value)
+            this.setState({errorMsg: dates.message})
+            //callback(this.state.value)
             return;
         }
         this.state.dates = dates
-        this.state.dataAvailable = true
-        this.setState({value: dates, dataAvailable: true })
-        callback(this.state.value)
+        await this.getDateA()
+        await this.getDateB() 
+        // this.state.dates = dates
+        // this.state.dataAvailable = true
+        this.setState({dataAvailable: true})
+        console.log("this is the state: " +  this.state)
+        //callback({value: dates, dataAvailable: true })
         return;
+    }
+
+    async getDateA (){
+        let dates = this.state.dates
+        let currentDate = Object.keys(dates)[this.state.dateIndexA];
+        let currentData = this.state.dates[currentDate];
+        currentData.date = currentDate
+        this.state.dataA = currentData
+        console.log("currentDataA: " + currentData)
+        return;
+    }
+    
+    async getDateB (){
+        let dates = this.state.dates
+        let currentDate = Object.keys(dates)[this.state.dateIndexB];
+        let currentData = this.state.dates[currentDate];
+        currentData.date = currentDate;
+        this.state.dataB = currentData;
+        return;
+    }
+
+    leftButtonA(){
+        if (this.state.dateIndexA + 1 < this.state.dateIndexB){
+            this.setState({dateIndexA: this.state.dateIndexA +1})
+        }
+    }
+
+    rightButtonA(){
+        if(this.state.dateIndexA > 0 & this.state.dateIndexA < this.state.dateIndexB)
+        this.setState({dateIndexA:  this.state.dateIndexA -=1})
+        // console.log("this is the state of the region: " + this.state)
+        
+    }
+
+    leftButtonB(){
+        let len = Object.keys(this.state.dates).length;
+        if (this.state.dateIndexB < len){
+            this.setState({dateIndexB: this.state.dateIndexB +1})
+        }
+        
+    }
+
+    rightButtonB(){
+        
+        if (this.state.dateIndexB -1 > this.state.dateIndexA)
+        this.setState({dateIndexB: this.state.dateIndexB -1})
     }
 
 render (){
     return(
         <div>
-            {/* {console.log(this.props.location.search)} */}
             <h1>My reagion search page</h1>
             <div id="regionData">
-                {console.log("region: " + this.state.value)}
+                {/* {console.log("region: " + this.state.value)} */}
                 {/* {this.state.value} */}
-                {this.state.dataAvailable ? <div> <SingleRegion dates={this.state.dates} position={0}></SingleRegion> <SingleRegion dates={this.state.dates} position={1}></SingleRegion> </div> : <div></div>
+                {this.state.dataAvailable ? 
+                    <div className="bundle">
+                    <div>
+                    <SingleRegion ref={this.SingleRegionaA} date={this.state.dataA}></SingleRegion>
+                    {/* <SingleRegion ref={this.SingleRegionaA} dates={this.state.dates} position={this.state.dateIndexA}></SingleRegion> */}
+                    <button onClick={this.leftButtonA.bind(this)}>Day before</button>
+                    <button onClick={this.rightButtonA.bind(this)}>Day after</button>
+                    </div>
+
+                    <div>
+                    
+                    {/* <SingleRegion ref={this.SingleRegionaB} dates={this.state.dates} position={this.state.dateIndexB}></SingleRegion> */}
+                    <button onClick={this.leftButtonB.bind(this)}>Day before</button>
+                    <button onClick={this.rightButtonB.bind(this)}>Day after</button>
+                    </div>
+                </div> 
+                : <div className="ErrorMsg">{this.state.errorMsg}</div>
                 }
             </div>
         </div>
--- reactWebsite/App/src/SingleRegion.js
+++ reactWebsite/App/src/SingleRegion.js
@@ -3,29 +3,62 @@ import React from 'react';
 class SingleRegion extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {currentDate: "", currentData: ""}
+        this.state = {currentDate: "", currentData: "", currentPosition: this.props.position}
+        // this.clickLeft = this.clickLeft.bind(this);
     }
 
     componentDidMount(){
-        let dates = this.props.dates
-        // console.log("this is the single dates: " + this.state.dates)
-        let currentDate = Object.keys(dates)[this.props.position];
-        let currentData = dates[currentDate];
+        let date = this.props.date
+        console.log("this is the single date: " , this.props.date)
+        //let currentDate = Object.keys(dates)[this.props.position];
+        //let currentData = dates[currentDate];
+        
+        let currentDate = date.date
+        delete date["date"];
+        let currentData = date
+        let deaths = currentData["deaths"];
+        console.log("the deaths: " + deaths)
         currentData = JSON.stringify(currentData)
-        this.setState({currentDate: currentDate, currentData: currentData})
-        console.log("this is the current: " + this.state.currentDate)
+        // this.setState({currentDate: currentDate, currentData: currentData})
+        this.setState({currentDate: currentDate, currentData: currentData, deaths: deaths})
+        // console.log("this is the current: " + this.state.currentDate)
+    }
+
+    // clickLeft() {
+    //     if (this.props.position > 0){
+    //         this.props.position -=1
+          
+    //     }
+    //     console.log("this is the new position" + this.props.position)
+    //     return
+    // }
+
+    componentDidUpdate(prevProps){
+        if (prevProps.position != this.props.position){
+            let date = this.props.date
+            //let currentDate = Object.keys(dates)[this.props.position];
+            let currentDate = Object.keys(date)[0];
+        let currentData = date[currentDate];
+        let deaths = currentData["deaths"];
+        console.log("the deaths: " + deaths)
+        currentData = JSON.stringify(currentData)
+        this.setState({currentDate: currentDate, currentData: currentData, deaths: deaths})
+        console.log("this is the current state of SingleRegion: " + this.state.currentDate)
+        }
     }
 
 render (){
+    
     return(
-        <div>
+        <div className="SingleRegion">
             {/* {console.log(this.props.location.search)} */}
             <p>single region</p>
-            <button>left</button>
-            {console.log(this.props)}
+            {/* <button onClick={this.clickLeft}>left</button> */}
+            {/* {console.log(this.props)} */}
             {this.state.currentDate}
             {this.state.currentData}
-            <button>right</button>
+            {this.state.deaths}
+            {/* <button>right</button> */}
             
         </div>
         );
