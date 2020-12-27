8525b4a4097e0d03140b4dcc5032dcdb5b3451a8
ragnarr18 <ragnarr18@ru.is>
2020-12-27 01:24:43 +0000

    port and host

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const {createServer} = require("http")
 const express = require('express')
 const app = express()
-const port = 9000 || process.env.PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 9000;
 // var router = express.Router();
 const fetch = require("node-fetch");
 const compression = require("compression")
@@ -69,4 +70,6 @@ async function getRegion(regionString, callback){
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
 })
-app.listen(process.env.PORT || 5000)
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
