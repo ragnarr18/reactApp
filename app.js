6ccc62c73dd9ea6855c71c51d4766b65480a4141
ragnarr18 <ragnarr18@ru.is>
2020-12-27 01:15:52 +0000

    sd

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.disable("x-powered-by")
 app.use(compression())
 app.use(morgan("common"))
 
-app.use(express.static(path.resolve(__dirname, "frontend/build")))
+app.use(express.static(path.resolve(__dirname, "client/build")))
 
 // }
 
@@ -67,6 +67,6 @@ async function getRegion(regionString, callback){
 }
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
 })
 app.listen(port)
